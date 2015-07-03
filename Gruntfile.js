d86cb0898569de5f96e49bd552a9ac559663ec0d
Tim Leerhoff <tleerhof@web.de>
2015-07-03 16:40:19 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+/* global module */
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        yuidoc: {
+            all: {
+                name: '<%= pkg.name %>',
+                description: '<%= pkg.description %>',
+                version: '<%= pkg.version %>',
+//        url: '<%= pkg.homepage %>',
+                options: {
+                    paths: ['lib/'],
+                    outdir: 'doc/'
+                }
+            }
+        },
+        jshint: {
+            all: ['lib/*.js', '*.js']
+        },
+        
+        mochacli: {
+          options: {
+            reporter: "nyan",
+            ui: "tdd"
+          },
+          all: ["test/tests.js"]
+        }    
+    });
+
+    grunt.loadNpmTasks("grunt-mocha-cli");
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-yuidoc');
+
+    grunt.registerTask('check', ['jshint']);
+    grunt.registerTask('test', ['mochacli']);
+    grunt.registerTask('default', ['jshint', 'mochacli', 'yuidoc']);
+};
